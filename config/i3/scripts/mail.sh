3d388ad6b763c79e1c170818a1c88496497a876b
Matt Critchlow <matt.critchlow@gmail.com>
2017-11-03 14:27:37 -0700

    i3blocks: check lastpass status before logging in

--- config/i3/scripts/mail.sh
+++ config/i3/scripts/mail.sh
@@ -2,6 +2,10 @@
 if tmux ls | grep 'mail' > /dev/null; then
   tmux attach-session -t mail
 else
-  lplogin
+  STATUS=$(lpass status)
+  if [ "$STATUS" == "Not logged in." ]; then
+    lplogin
+  fi
+
   "$HOME/.tmux-projects/mail"
 fi
