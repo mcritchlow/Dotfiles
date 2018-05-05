079eb16ecdddc2d4d39e840d68d972e0476f1fc9
Matt Critchlow <matt.critchlow@gmail.com>
2018-05-04 20:12:00 -0700

    Remove unused config files

--- config/i3/scripts/lock.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-# idea taken from https://www.reddit.com/r/i3wm/comments/5ag6y7/psa_if_youre_using_imagemagickgraphicsmagick_to/
-image_file=/tmp/screen_lock.png
-resolution=$(xdpyinfo | grep dimensions | awk '{print $2}')
-filters='noise=alls=20,scale=iw*.05:-1,scale=iw*20:-1:flags=neighbor'
-ffmpeg -y -loglevel 0 -s "$resolution" -f x11grab -i "$DISPLAY" -vframes 1 \
-  -vf "$filters" "$image_file"
-i3lock -e -i "$image_file"
