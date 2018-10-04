b83485150729c4f9b24e6cd9fafc2e5872c1b4a6
Matt Critchlow <matt@critchlow.xyz>
2018-10-04 09:06:02 -0700

    Switch to nvidia only for Dell laptop

--- screenlayout/precision-fancy-ass-monitor-hidpi.sh
+++ screenlayout/precision-fancy-ass-monitor-hidpi.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 # attach to hidpi monitor and turn off the laptop monitor.
 # eventually turn into something more dynamic
-xrandr --output DP-1 --auto --scale 2x2 --right-of eDP-1-1 && xrandr --output eDP-1-1 --auto --off
+xrandr --output DP-1 --auto --scale 2x2 --right-of DP-4 && xrandr --output DP-4 --auto --off
