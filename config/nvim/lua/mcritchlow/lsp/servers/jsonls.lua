40d8deb4fdeaa7c6190f35e147fb9f6291ad1ee3
Matt Critchlow <matt@critchlow.xyz>
2022-05-18 06:03:37 -0700

    nvim: get jsonls diagnostics working
    Needed to specify `validate = true` because apparently these defaults
    get flipped when setting up the `schemas`..
    
    https://github.com/b0o/SchemaStore.nvim/issues/8#issuecomment-1129528787

--- config/nvim/lua/mcritchlow/lsp/servers/jsonls.lua
+++ config/nvim/lua/mcritchlow/lsp/servers/jsonls.lua
@@ -1,6 +1,6 @@
 local M = {}
 M.setup = function(on_attach, capabilities)
-    local lspconfig = require "lspconfig"
+    local lspconfig = require("lspconfig")
     local json_caps = capabilities
     json_caps.textDocument.completion.completionItem.snippetSupport = true
 
@@ -12,6 +12,8 @@ M.setup = function(on_attach, capabilities)
         capabilities = json_caps,
         settings = {
             json = {
+                format = { enable = true },
+                validate = { enable = true },
                 schemas = require('schemastore').json.schemas(),
             }
         }
