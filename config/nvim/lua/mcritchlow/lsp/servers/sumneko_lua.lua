8bded3ab8c6e289d8f1a337ff583270be931e33f
Matt Critchlow <matt@critchlow.xyz>
2022-05-23 07:03:05 -0700

    nvim: properly setup luadev plugin
    This was missing integration with sumneko_lua LSP. Now, works great!

--- config/nvim/lua/mcritchlow/lsp/servers/sumneko_lua.lua
+++ config/nvim/lua/mcritchlow/lsp/servers/sumneko_lua.lua
@@ -1,47 +1,19 @@
 local M = {}
 
-local settings = {
-    Lua = {
-        runtime = {
-            version = "LuaJIT",
-        },
-        diagnostics = {
-            globals = {
-                "vim",
-                "use",
-                "describe",
-                "it",
-                "assert",
-                "before_each",
-                "after_each",
-            },
-        },
-        disable = {
-            "lowercase-global",
-            "undefined-global",
-            "unused-local",
-            "unused-function",
-            "unused-vararg",
-            "trailing-space",
-        },
-    },
-}
-
 M.setup = function(on_attach, capabilities)
-    local lspconfig = require "lspconfig"
-
-    lspconfig.sumneko_lua.setup {
-        on_attach = function(client, bufnr)
-            client.server_capabilities.document_formatting = false
-            client.server_capabilities.document_range_formatting = false
-            on_attach(client, bufnr)
-        end,
-        settings = settings,
+    local lspconfig = require("lspconfig")
+    local luadev = require("lua-dev").setup({
+      -- add any options here, or leave empty to use the default settings
+      lspconfig = {
+        capabilities = capabilities,
+        on_attach = on_attach,
         flags = {
             debounce_text_changes = 150,
         },
-        capabilities = capabilities,
-    }
+      },
+    })
+
+    lspconfig.sumneko_lua.setup(luadev)
 end
 
 return M
