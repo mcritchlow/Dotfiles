5a2e51fa574927e3eab5ed9ff54fcaa16e0064df
Matt Critchlow <matt@critchlow.xyz>
2022-07-22 14:29:58 -0700

    nvim: fix null-ls standardrb formatter
    Need to set `cwd` for surfliner

--- config/nvim/lua/mcritchlow/utils/init.lua
+++ config/nvim/lua/mcritchlow/utils/init.lua
@@ -34,6 +34,9 @@ _M.setup_null_ls = function(service, cwd_suffix)
             args = { service, "bundle", "exec", "standardrb", " --no-fix", "-f", "json", "--stdin", "$FILENAME" },
         }),
         null_ls.builtins.formatting.standardrb.with({
+            cwd = function(params)
+                return util.root_pattern(".git")(vim.fn.getcwd()) .. cwd_suffix
+            end,
             command = "docker-compose-exec",
             args = { service, "bundle", "exec", "standardrb", " --fix", "--format", "quiet", "--stderr", "--stdin", "$FILENAME" },
         }),
@@ -76,6 +79,57 @@ end
 -- utils.replace_term_codes("<Esc>")
 _M.replace_term_codes = function(value)
     return vim.api.nvim_replace_termcodes(value, true, true, true)
+end
+
+-- Default LSP keymaps
+_M.lsp_keymaps = function(bufnr)
+  -- TODO: finish setting up which-key for lsp buffer mappings
+  local wk = require('which-key')
+  -- normal mode
+  local wk_opts = {
+      mode = "n",
+      buffer = bufnr,
+      noremap = true,
+      nowait = true,
+      prefix = "",
+      silent = true,
+  }
+  local telescope = require('telescope.builtin')
+  local opts = { buffer = bufnr }
+  vim.api.nvim_buf_set_option(bufnr, 'omnifunc', 'v:lua.vim.lsp.omnifunc')
+  --
+  wk.register({
+    ["<leader>l"] = {
+      D = { "<cmd>lua vim.lsp.buf.declaration()<cr>", "[LSP] Hover" },
+      d = { "<cmd>Telescope lsp_definitions<cr>", "[LSP] Definitions" },
+      td = { "<cmd>lua vim.lsp.buf.type_definition()<cr>", "[LSP] Type Definition" },
+      h = { "<cmd>lua vim.lsp.buf.hover()<cr>", "[LSP] Hover" },
+      n = { "<cmd>lua vim.lsp.buf.rename()<cr>", "[LSP] Rename" },
+      r = { "<cmd>Telescope lsp_references<cr>", "[LSP] References" },
+      i = { "<cmd>Telescope lsp_implementations<cr>", "[LSP] Implementations" },
+      ca = { "<cmd>lua vim.lsp.buf.code_action()<cr>", "[LSP] Code Actions" },
+      so = { "<cmd>Telescope lsp_document_symbols<cr>", "[LSP] Document Symbols" },
+      sh = { "<cmd>lua vim.lsp.buf.signature_help()<cr>", "[LSP] Signature Help" },
+    }
+  }, wk_opts)
+
+  -- wk.reg
+
+  -- vim.keymap.set('n', 'gD', vim.lsp.buf.declaration, opts)
+  -- vim.keymap.set('n', 'gd', telescope.lsp_definitions, opts)
+  -- vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
+  -- vim.keymap.set('n', 'gi', telescope.lsp_implementations, opts)
+  -- vim.keymap.set('n', '<C-k>', vim.lsp.buf.signature_help, opts)
+  -- vim.keymap.set('n', '<leader>wa', vim.lsp.buf.add_workspace_folder, opts)
+  -- vim.keymap.set('n', '<leader>wr', vim.lsp.buf.remove_workspace_folder, opts)
+  -- vim.keymap.set('n', '<leader>wl', function()
+  --   vim.inspect(vim.lsp.buf.list_workspace_folders())
+  -- end, opts)
+  -- vim.keymap.set('n', '<leader>D', vim.lsp.buf.type_definition, opts)
+  -- vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  -- vim.keymap.set('n', 'gr', telescope.lsp_references, opts)
+  -- vim.keymap.set('n', '<leader>ca', vim.lsp.buf.code_action, opts)
+  -- vim.keymap.set('n', '<leader>so', telescope.lsp_document_symbols, opts)
 end
 
 return _M
