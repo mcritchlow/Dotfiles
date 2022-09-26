770bd1dcd8369d1a0f483084a2ade9098eab86a8
Matt Critchlow <matt@critchlow.xyz>
2022-09-26 08:49:00 -0700

    nvim: use light_default theme variant
    Better highlights for symbols. Seems nicer

--- config/nvim/lua/mcritchlow/theme.lua
+++ config/nvim/lua/mcritchlow/theme.lua
@@ -4,7 +4,7 @@ _M.init = function()
   local themeLoaded, github = pcall(require, "github-theme")
   if themeLoaded then
     github.setup({
-      theme_style = "light",
+      theme_style = "light_default",
       hide_inactive_statusline = false,
       sidebars = {'NvimTree'},
     })
